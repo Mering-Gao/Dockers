8aea9005b95d711e90bea2df5e757a2b3fc63f56
mering <canhuayin@gmail.com>
2020-10-04 01:03:53 +0800

    update

---
+++ redis/cluster/start/start-cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REPLICAS=1
+HOSTS=""
+
+for HOSTNAME in "redis-cluster-1" "redis-cluster-2" "redis-cluster-3" "redis-cluster-4" "redis-cluster-5" "redis-cluster-6";
+do
+IP=$(getent hosts $HOSTNAME | awk '{ print $1 }')
+HOSTS="$HOSTS $IP:6379"
+done
+
+echo "yes"| redis-cli --cluster create $HOSTS --cluster-replicas 1
+
+redis-server
